a39a70f9b471983f74f13bbf21e18503828a972a
lauragalvan29 <galvanmarquezlaura@gmail.com>
2024-12-03 20:28:05 +0100

    pruevas-untiaria

---
+++ __test__/esmayuscula.test.js
@@ -0,0 +1,10 @@
+const isUpperCase = require('../utils/esmayuscula');
+
+test('Comprobar mayuscula True', () => {
+    expect(isUpperCase("HOLA")).toBe(true);
+})
+
+test('Comprobar mayuscula False', () => {
+    expect(isUpperCase("hola")).toBe(false);
+})
+
---
+++ __test__/suma.test.js
@@ -0,0 +1,19 @@
+const sum = require('../utils/suma');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(3);
+})
+
+test('adds -1 + 2 to equal 3', () => {
+    expect(sum(-1,2)).toBe(1);
+})
+describe('calculator', () => {
+    test.each`
+      firstValue | secondValue | expectedResult    
+      ${3}       | ${1}        | ${4}
+      ${-4}      | ${1}        | ${-3}
+      ${-2}      | ${1}        | ${-1}
+    `('$firstValue + $secondValue should return $expectedResult', ({firstValue, secondValue, expectedResult}) => {
+      expect(sum(firstValue, secondValue)).toBe(expectedResult);
+    });
+  })
